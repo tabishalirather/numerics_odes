e8f1dcdd350a67cbd481e2f3b763b45005c0e7bf
tabishalirather <tabishrather7006@gmail.com>
2026-06-04 08:55:07 +0200

    comit before starting project2

---
+++ Project2/source_files/__main__.py
@@ -0,0 +1,151 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from exercise import *
+
+#---------------------------------------------------------------------
+# main program
+#---------------------------------------------------------------------
+
+V = 25   # biochemical reactor volume [m^3]                  
+Q1 = 2.5 # discharge of the first pipe [m^3/min]
+Q2 = 2.5 # discharge of the second pipe [m^3/min]
+cz0 = 20 # zombie virus concentration in the first pipe [mg/m^3] 
+cc0 = 25 # chemical concentration in the second pipe [mg/m^3]   
+
+k = 1.2  # reaction rate constant [m^3/(mg*min)]
+t0 = 0 
+te = 30  # integration interval = 30 minutes
+y0 = np.array([0,0,0]) # initial condition y0 = [0,0,0] 
+
+# Parameters for Adams-Bashforth-2 two-step method
+a_AB2 = np.array([0, -1, 1])
+b_AB2 = np.array([-0.5, 1.5, 0]) 
+        
+# Parameters for Adams-Moulton-2 two-step method
+a_AM2 = np.array([0, -1, 1])
+b_AM2 = np.array([-1/12, 2/3, 5/12])
+  
+# Parameters for BDF-2 two-step method 
+a_BDF2 = np.array([1/2,-2,3/2])
+b_BDF2 = np.array([0,0,1])   
+
+num_solvers = 5
+#h = np.array([0.1, 0.01, 1e-3, 1e-4]) 
+h = np.array([0.1, 0.01]) 
+cp_err = np.zeros((len(h),num_solvers))
+
+for i in range(len(h)): 
+  h_ = h[i]
+  t = np.arange(t0,te,h_)
+  t = np.append(t,te)
+      
+  y_app = odeint(f,y0,t, tfirst = True)              
+  y_euler_expl = euler_expl(f,y0,t)  
+  y_euler_impr = euler_impr(f,y0,t)   
+  y_AB2 = twostep(f,[],y0,[],t,a_AB2,b_AB2)
+  y_AM2 = twostep(f,Jf,y0,[],t,a_AM2,b_AM2)        
+  y_BDF2 = twostep(f,Jf,y0,[],t,a_BDF2,b_BDF2)
+            
+  col = 2 
+  # absolute error for vaccine product concentration
+  cp_err[i,:] = np.array([np.max(np.abs(y_euler_expl[:,col] - y_app[:,col])), 
+                          np.max(np.abs(y_euler_impr[:,col] - y_app[:,col])), 
+                          np.max(np.abs(y_AB2[:,col]        - y_app[:,col])), 
+                          np.max(np.abs(y_AM2[:,col]        - y_app[:,col])), 
+                          np.max(np.abs(y_BDF2[:,col]       - y_app[:,col]))])
+
+# Display y(te) for final step size 
+m = len(t)-1
+print('Concentrations c = (c_Z,c_C,c_P) at t_end = 30 min for h = ',h_,'\n')
+print('     odeint : ',y_app[m,:])  
+print('expl. Euler : ',y_euler_expl[m,:]) 
+print('impl. Euler : ',y_euler_impr[m,:])  
+print('        AB2 : ',y_AB2[m,:])  
+print('        AM2 : ',y_AM2[m,:])  
+print('       BDF2 : ',y_BDF2[m,:])  
+
+#--------------------------------------------------------------------------
+# plot concentrations c_z(t), c_C(t), c_P(t)  [for h(end)]
+#--------------------------------------------------------------------------
+path = "/home/student/result/" # <--- change this path to that of your folder
+fs = 15 # FontSize
+title = ['Virus concentration','Chemical concentration','Vaccine product concentration']
+ylabel = ['$c_Z$','$c_C$','$c_P$']
+fname = ['c_Z.png','c_C.png','c_P.png']
+for i in range(3):
+  plt.figure()
+  plt.rcParams.update({'font.size': fs})
+  plt.plot(t,y_euler_expl[:,i],t,y_euler_impr[:,i],t,y_AB2[:,i],t,y_AM2[:,i],t,y_BDF2[:,i],t,y_app[:,i],'--k')
+  plt.legend(['Explicit Euler','Improved Euler','AB2','AM2','BDF2','odeint'],loc = 'best')
+  plt.title(title[i])
+  plt.xlabel('t')
+  plt.ylabel(ylabel[i])
+  plt.savefig(path + fname[i])
+
+#--------------------------------------------------------------------------
+# plot global error c_P(t)
+#--------------------------------------------------------------------------
+plt.figure()
+plt.rcParams.update({'font.size': fs})
+plt.loglog(h,cp_err[:,0],'b',h,cp_err[:,1],'r',h,cp_err[:,2],'g',h,cp_err[:,3],'k',h,cp_err[:,4],'m')
+plt.title('Error vs stepsize for c_P')
+plt.legend(['Explicit Euler','Improved Euler','AB2','AM2','BDF2'],loc = 'best')
+plt.xlabel('h')
+plt.ylabel('Error')
+plt.grid()
+plt.savefig(path + 'error.png')            
+
+#--------------------------------------------------------------------------
+# robustness
+#-------------------------------------------------------------------------- 
+h_ = 0.2
+t = np.arange(t0,te,h_)
+t = np.append(t,te)
+
+y_euler_expl = euler_expl(f,y0,t)  
+y_euler_impr = euler_impr(f,y0,t)
+y_AB2 = twostep(f,[],y0,[],t,a_AB2,b_AB2)
+y_AM2 = twostep(f,Jf,y0,[],t,a_AM2,b_AM2)
+y_BDF2 = twostep(f,Jf,y0,[],t,a_BDF2,b_BDF2)
+
+#alpha = np.arange(0.5,1.5,0.05)
+alpha = np.arange(0.7,1.3,0.05)
+delta = np.zeros((num_solvers,len(alpha)))
+j = 0 # c_z
+#j = 1 # c_c
+#j = 2 # c_P
+
+k_ = np.array([-1,-1,2])*k  
+
+for i in range(len(alpha)):     
+  Q_in = np.array([alpha[i]*Q1,Q2,0]) # variation of Q_in(1) = Q1 by at most +/- 30% 
+  Q_out = Q_in[0] + Q_in[1] 
+  c_in = np.array([cz0,cc0,0] )
+  k_ = np.array([-1,-1,2])*k     
+  f_ = lambda  t,c: (Q_in/V) * c_in + k_ * c[0] * c[1] - Q_out/V * c 
+  Jf_ = lambda t,c: np.matrix([[k_[0]*c[1] - Q_out/V , k_[0]*c[0]           , 0      ], 
+                               [k_[1]*c[1]           , k_[1]*c[0] - Q_out/V , 0      ], 
+                               [k_[2]*c[1]           , k_[2]*c[0]           ,-Q_out/V]])
+    
+  y_euler_expl_ = euler_expl(f_,y0,t)  
+  y_euler_impr_ = euler_impr(f_,y0,t)
+  y_AB2_ = twostep(f_,[],y0,[],t,a_AB2,b_AB2)
+  y_AM2_ = twostep(f_,Jf_,y0,[],t,a_AM2,b_AM2)
+  y_BDF2_ = twostep(f_,Jf_,y0,[],t,a_BDF2,b_BDF2)
+    
+  delta[0,i] = np.linalg.norm(y_euler_expl[:,j] - y_euler_expl_[:,j])
+  delta[1,i] = np.linalg.norm(y_euler_impr[:,j] - y_euler_impr_[:,j])
+  delta[2,i] = np.linalg.norm(y_AB2[:,j]        - y_AB2_[:,j])
+  delta[3,i] = np.linalg.norm(y_AM2[:,j]        - y_AM2_[:,j])
+  delta[4,i] = np.linalg.norm(y_BDF2[:,j]       - y_BDF2_[:,j])
+
+plt.figure()
+plt.rcParams.update({'font.size': fs})
+plt.plot(alpha,delta[0,:],'r-o',alpha,delta[1,:],'g-*',alpha,delta[2,:],'b-*',alpha,delta[3,:],'m-o',alpha,delta[4,:],'k-*')
+plt.legend(['Explicit Euler','Improved Euler','AB2','AM2','BDF2'],loc = 'best')
+plt.title('Robustness study by changing $Q_{1,new} = \\alpha Q_1$')
+plt.xlabel('$\\alpha$')
+ylabel = ['$\|\Delta c_Z\|$','$\|\Delta c_C\|$','$\|\Delta c_P\|$']
+plt.ylabel(ylabel[j])   
+plt.savefig(path + 'robustness.png')
---
+++ Project2/source_files/exercise.py
@@ -0,0 +1,233 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+#-------------------------------------------------------------------------------------------
+# Constants (do not change!)
+#-------------------------------------------------------------------------------------------
+V = 25   # biochemical reactor volume [m^3]                  
+Q1 = 2.5 # discharge of the first pipe [m^3/min]
+Q2 = 2.5 # discharge of the second pipe [m^3/min]
+cz0 = 20 # virus concentration in the first pipe [mg/m^3] 
+cc0 = 25 # chemical concentration in the second pipe [mg/m^3]   
+k = 1.2  # reaction rate constant [m^3/(mg*min)]
+
+#-------------------------------------------------------------------------------------------
+# (1) Template for the explicit Euler method that you shall complete.
+#-------------------------------------------------------------------------------------------
+def euler_expl(f,y0,t):
+  """
+  euler_expl(f,y0,t)    
+  
+  This function implements the explicit Euler method for solving the initial value problem
+  
+    y'(s) = f(s,y(s))  for s in [t0,te]        
+    y(t0) = y0  
+    
+  where t0 is the first and te is the last entry in the array t.
+    
+  Parameters
+  -----------------
+  f  : callable(t,y)
+       Computes the derivative of y at t, i.e., computes the right-hand side of y' = f(t,y). 
+  y0 : numpy.array
+       Initial condition on y (can be a vector).
+  t  : numpy.array
+       A sequence of time points for which to solve for y. 
+       The initial value point must be the first element of this sequence. 
+       This sequence must be monotonically increasing. 
+       
+  Returns
+  -----------------
+  y : numpy.array, shape (len(t), len(y0))
+      Array containing the value of y for each desired time in t, 
+      with the initial value y0 in the first row.
+    
+  Example
+  -----------------
+  >>> f = lambda t,y: y/2
+  >>> y0 = np.array([1])
+  >>> t = np.linspace(0,3,100)
+  >>> y = euler_expl(f,y0,t)  
+  """   
+  
+  ####################
+  #edit the code here#
+  ####################
+    
+  return ##
+
+#-------------------------------------------------------------------------------------------
+# (2) Template for the improved Euler method that you shall complete.
+#-------------------------------------------------------------------------------------------
+def euler_impr(f,y0,t):
+  """
+  euler_impr(f,y0,t)    
+  
+  This function implements the explicit Euler method for solving the initial value problem
+  
+    y'(s) = f(s,y(s))  for s in [t0,te]        
+    y(t0) = y0  
+    
+  where t0 is the first and te is the last entry in the array t.
+    
+  Parameters
+  -----------------
+  f  : callable(t,y)
+       Computes the derivative of y at t, i.e., computes the right-hand side of y' = f(t,y). 
+  y0 : numpy.array
+       Initial condition on y (can be a vector).
+  t  : numpy.array
+       A sequence of time points for which to solve for y. 
+       The initial value point must be the first element of this sequence. 
+       This sequence must be monotonically increasing. 
+       
+  Returns
+  -----------------
+  y : numpy.array, shape (len(t), len(y0))
+      Array containing the value of y for each desired time in t, 
+      with the initial value y0 in the first row.
+    
+  Example
+  -----------------
+  >>> f = lambda t,y: y/2
+  >>> y0 = np.array([1])
+  >>> t = np.linspace(0,3,100)
+  >>> y = euler_impr(f,y0,t)  
+  """  
+  
+  ####################
+  #edit the code here#
+  ####################
+    
+  return ##
+
+#-------------------------------------------------------------------------------------------
+# (3) Template for a general explicit or implicit two-step method that you shall complete.
+#-------------------------------------------------------------------------------------------
+def twostep(f,Jf,y0,y1,t,a,b):
+  """
+  twostep(f,y0,y1,t,a,b)    
+  
+  This function implements an explicit or implicit two-step method for solving the initial value problem
+  
+    y'(s) = f(s,y(s))  for s in [t0,te]        
+    y(t0) = y0  
+    
+  where t0 is the first and te is the last entry in the array t.
+    
+  Parameters
+  -----------------
+  f  : callable(t,y)
+       Computes the derivative of y at t, i.e., computes the right-hand side of y' = f(t,y). 
+  Jf : callable(t,y) for implicit method, and [] (empty) for explicit method
+       For an implicit method JF is the Jacobian of f with respect to y,
+       i.e., Jf(t,y) = df/dy(t,y).
+  y0 : numpy.array
+       Initial condition on y (can be a vector).
+  y1 : numpy.array
+       approximate solution for the second time grid point t[1].      
+       This is an optional parameter, i.e., if y1 is empty,   
+       then an approximate value for y1 is computed.
+  t  : numpy.array
+       A sequence of time points for which to solve for y. 
+       The initial value point must be the first element of this sequence. 
+       This sequence must be monotonically increasing. 
+  a,b: Parameters of the two-step method
+         a_2*y_{n+2} + a_1*y_{n+1} + a_0*y_n = 
+         h * (b_0*f(t_n,y_n) + b_1*f(t_{n+1},y_{n+1}) + b_2*f(t_{n+2},y_{n+2}))         
+       In case of an explicit method a_2 = 1 and b_2 = 0 (or len(b)=2) is assumed.
+      
+  Returns
+  -----------------
+  y : numpy.array, shape (len(t),len(y0))
+      Array containing the value of y for each desired time in t, 
+      with the initial value y0 in the first row.
+    
+  Example
+  -----------------
+  >>> f  = lambda t,y: y/2             # y' = f(t,y,c) = y/2
+  >>> Jf = lambda t,y: 1/2             # df/dy = 1/2
+  >>> y0 = np.array([1])               # y0 = y(0) = 1 ---> y(t) = exp(t/2)                   
+  >>> t = np.linspace(0,3,100)   
+  >>> # explicit two-step method 
+  >>> a = np.array([0, -1, 1])         # Adams-Bashforth-2 two-step method
+  >>> b = np.array([-0.5, 1.5, 0])     # Adams-Bashforth-2 two-step method       
+  >>> y = twostep(f,[],y0,[],t,a,b)    
+  >>> # implicit two-step method      
+  >>> a = np.array([0, -1, 1])         # Adams-Moulton-2 two-step method
+  >>> b = np.array([-1/12, 2/3, 5/12]) # Adams-Moulton-2 two-step method      
+  >>> y = twostep(f,Jf,y0,[],t,a,b)   
+  """ 
+ 
+  ####################
+  #edit the code here#
+  ####################
+    
+  return ##
+
+#-------------------------------------------------------------------------------------------
+# (4) Template for the ODE function that you shall complete.
+#-------------------------------------------------------------------------------------------
+def f(t,c):
+  """
+  f(t,c)    
+  
+  This function implements the right-hand side of the sought ODE c' = f(t,c) 
+  for the biochemical reaction considered in Project 2, see the project 
+  description sheet in StudIP for details.    
+    
+  Parameters
+  -----------------
+  t : numpy.array, real number 
+      time point(s)        
+  c : numpy.array 
+      c = [cz,cc,cp]
+      cz : zombie virus concentration 
+      cc : chemical concentration
+      cp : vaccine product concentration
+      
+  Returns
+  -----------------
+  dcdt : numpy.array
+         dcdt = f(t,c) = [dcz_dt,dcc_dt,dcp_dt]      
+  """ 
+  ####################
+  #edit the code here#
+  ####################
+    
+  return ##
+
+#-------------------------------------------------------------------------------------------
+# (5) Template for the Jacobian of the ODE function that you shall complete.
+#-------------------------------------------------------------------------------------------
+def Jf(t,c):
+  """
+  Jf(t,c)    
+  
+  This function implements the Jacobian of the previously defined function f(t,c),
+  i.e., Jf(t,c) is the derivative of f(t,c) with respect to c. 
+    
+  Parameters
+  -----------------
+  t : real number  
+      time point        
+  c : numpy.array 
+      c = [cz,cc,cp]
+      cz : zombie virus concentration 
+      cc : chemical concentration
+      cp : vaccine product concentration
+      
+  Returns
+  -----------------
+  dfdc : numpy.array shape (3,3) , i.e., 3-by-3 matrix 
+         dfdc = [df[0]_dc[0], df[0]_dc[1], df[0]_dc[2], 
+                 df[1]_dc[0], df[1]_dc[1], df[1]_dc[2],
+                 df[2]_dc[0], df[2]_dc[1], df[2]_dc[2]]      
+  """ 
+
+  ####################
+  #edit the code here#
+  ####################
+    
+  return ##
