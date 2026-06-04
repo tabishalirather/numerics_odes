8022653b2dfb1d4caccfbc8dba12ab1ea1566406
tabishalirather <tabishrather7006@gmail.com>
2026-06-04 08:52:12 +0200

    comit before starting project2

--- week6/code/solver.py
+++ week6/code/solver.py
@@ -18,10 +18,27 @@ t= np.linspace(t_start, t_end, int((t_end-t_start)/h))
 
 improved_euler_solution = improved_euler(f,ic,t)
 heun_solution = heun(f,ic,t)
-analytic_solution = analytic_solution(t)
+analytic_solution_model = analytic_solution(t)
 plt.figure(1)
 # plt.plot(t,analytic_solution,'k')
 
-plt.plot(t,analytic_solution,'k', t, improved_euler_solution, 'r', t, heun_solution, '--', 'g')
-# plt.legend([])
+plt.plot(t,analytic_solution_model,'k',  t, improved_euler_solution, 'r', t, heun_solution, '--', 'g')
+plt.legend(['Analytic', 'Improved Euler', 'Heun'])
+
+h=np.logspace(-3,-1,10)  #starts from 10e-3 to 10e-1, log steps
+err_euler_improved = np.zeros(len(h))
+err_heun = np.zeros(len(h))
+
+for i in range(len(h)):
+    t = np.linspace(t_start, t_end, int((t_end-t_start)/h[i]))
+    improved_euler_solution = improved_euler(f, ic, t)
+    heun_solution = heun(f, ic, t)
+    analytic_solution = analytic_solution(t)
+
+    err_euler_improved = np.max(abs(analytic_solution-improved_euler_solution))
+    err_heun = np.max(analytic_solution-heun_solution)
+
+plt.figure(2)
+plt.loglog(h,err_euler_improved, h, err_heun, h, h*h)
 plt.show()
+
